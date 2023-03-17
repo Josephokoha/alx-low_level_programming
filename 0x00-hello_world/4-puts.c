042c499c8d6034b25f63a26304c8df1257d4988b
Joseph Okoha <thejosephokoha@gmail.com>
2023-03-17 07:12:54 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
  * main - this is the main function
  *
  * Return: always 0
-**/
+ */
 
 int main(void)
 {
-	put("\"programming is like building a multilingual puzzle);
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
