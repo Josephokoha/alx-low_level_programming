5d952e62b8a15bf0f0781112c983a99d68b6c7f9
Joseph Okoha <thejosephokoha@gmail.com>
2023-03-29 01:08:21 +0100

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - this change the value of n to 98
+ * @n: this is the value to be replacced
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this id the read me filr for this directory 0x05-pointers_arrays_strings
